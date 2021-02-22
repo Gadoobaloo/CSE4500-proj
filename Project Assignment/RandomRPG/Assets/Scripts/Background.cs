503a5c88a5542a1f889e3da1ce214b98fd544961
Angel Zuniga <zangle982@gmail.com>
2021-02-22 09:29:03 -0800

    mid week progress

---
+++ Project Assignment/RandomRPG/Assets/Scripts/Background.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public Enviroment BgEnvironment;
+
+    List<Sprite> sprites = new List<Sprite>();
+}
---
+++ Project Assignment/RandomRPG/Assets/Scripts/GameInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Enviroment { None, Beach, Castle, City, Desert, Forest, Snow, Space, Underwater, Volcano }
+public enum PlayType { Playable, NPC }
+public enum AttackTarget { SingleOpponent, AllOpponents, Self, SingleAlly, AllAllies }
+
+public class GameInfo : MonoBehaviour
+{
+
+}
