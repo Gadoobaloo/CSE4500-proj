9da089890e2b45d1793e54a738d7c5bd0f750638
Angel Zuniga <zangle982@gmail.com>
2021-03-31 12:22:39 -0700

    API Assignment done

--- API test/api_test.js
+++ API test/api_test.js
@@ -1,1 +1,11 @@
-//TODO: Retrieve info in JSON format from a public API.
+//TODO: Retrieve info in JSON format from a public API. -- DONE
+
+function getInfo(){
+    fetch('https://www.boredapi.com/api/activity')
+        .then(response => response.json())
+        .then(json => {
+            console.log('json object from API:')
+            console.log(json)
+            document.getElementById('activityText').innerHTML=json.activity;
+        })
+}
